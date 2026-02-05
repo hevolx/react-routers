059dfdb65022335e10bb8d56b199a7725929d8dc
Love Holmgren <143357495+hevolx@users.noreply.github.com>
2026-02-05 07:12:56 +0100

    [feat] added react-router

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,18 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 export default function Footer() {
 
   // get the navigate function
+    const navigate = useNavigate()
 
   const goBack = () => {
     // imperatively redirect back
-    
+    navigate(-1);
   }
 
   const goForward = () => {
     // imperatively redirect forward
-    
+    navigate(1);
   }
 
   return (
@@ -19,5 +20,5 @@ export default function Footer() {
       <button onClick={goBack}>Back</button>
       <button onClick={goForward}>Forward</button>
     </footer>
-  );
+  )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
@@ -6,14 +7,21 @@ import { selectCurrentUser, selectIsLoggedIn } from "../features/session/session
 export default function Profile () {
   const currentUser = useSelector(selectCurrentUser)
   const loggedIn = useSelector(selectIsLoggedIn);
-  
-  // use loggedIn to return a Navigate
 
-  return (
-    <main>
-      <h1>{currentUser.username}</h1>
-      <Link to={`edit`}>Edit</Link>
-      {/* Tell React Router where to render child routes` */}
-    </main>
-  )
+  // use loggedIn to return a Navigate
+  const UserProfile = ({ loggedIn }) => {
+    if (!loggedIn) {
+      return (
+        <Navigate to='/' />
+      )
+    }
+
+    return (
+      <main>
+        <h1>{currentUser.username}</h1>
+        <Link to={`edit`}>Edit</Link>
+        {/* Tell React Router where to render child routes` */}
+      </main>
+    )
+  }
 }
